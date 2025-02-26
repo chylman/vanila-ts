00381e25f6f758339f9b3bf78b1258458ee6e3c0
Ilja Afanasev <chylmanprosto@gmail.com>
2025-02-26 12:31:51 +0300

    feat: add l 02

---
+++ src/02/02.ts
@@ -0,0 +1,52 @@
+export type CityType = {
+    title: string
+    countryTitle: string
+}
+
+type AddressType = {
+    streetTitle: string
+    city: CityType
+}
+
+type TechType = {
+    id: number
+    title: string
+}
+
+type StudentType = {
+    id: number
+    name: string
+    age: number
+    isActive: boolean
+    address: AddressType
+    technologies: Array<TechType>
+}
+
+export const student: StudentType = {
+    id: 1,
+    name: "Ilja",
+    age: 32,
+    isActive: false,
+    address: {
+        streetTitle: 'Surganova 2',
+        city: {
+            title: 'Minsk',
+            countryTitle: 'Belarus',
+        }
+    },
+    technologies: [
+        {
+            id: 1,
+            title: 'HTML'
+        },
+        {
+            id: 2,
+            title: 'CSS'
+        },
+        {
+            id: 3,
+            title: 'React'
+        },
+    ]
+}
+
