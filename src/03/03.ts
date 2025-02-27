5ac5d1cac2c6684133a5441617156805b17e95b1
Ilja Afanasev <chylmanprosto@gmail.com>
2025-02-27 15:55:33 +0300

    feat: add l 03

---
+++ src/03/03.ts
@@ -0,0 +1,31 @@
+import {StudentType} from "../02/02.ts";
+
+export const sum = (a: number, b: number) => {
+    return a + b;
+}
+
+export function sum2 (a: number, b: number) {
+    return a + b;
+}
+
+export const addSkill = (st: StudentType, skill: string) => {
+    st.technologies.push({
+        id: new Date().getTime(),
+        title: skill,
+    })
+}
+
+export function addSkill2 (st: StudentType, skill: string) {
+    st.technologies.push({
+        id: new Date().getTime(),
+        title: skill,
+    })
+}
+
+export function makeStudentActive (s: StudentType) {
+    s.isActive = true;
+}
+
+export const doesStudentLiveIn = (s: StudentType, cityName: string) => {
+    return s.address.city.title === cityName;
+}
